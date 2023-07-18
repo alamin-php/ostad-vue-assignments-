6b5230297426a1c1015a4de06d9097e20c4af721
alamin-php <alamin.php100@gmail.com>
2023-07-18 23:44:59 +0600

    module-3 added

---
+++ Module-3/src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import Footer from './components/Footer.vue';
+import About from './components/About.vue';
+import Contact from './components/Contact.vue';
+import New from './components/New.vue';
+import Testimonial from './components/Testimonial.vue';
+import Pricing from './components/Pricing.vue';
+import Product from './components/Product.vue';
+import GrowBusiness from './components/GrowBusiness.vue';
+import DigitalService from './components/DigitalService.vue';
+import LineWrap from './components/LineWrap.vue';
+import MobileMenu from './components/MobileMenu.vue';
+
+
+</script>
+
+<template>
+  <LineWrap />
+  <MobileMenu />
+  <Navbar />
+  <Hero />
+  <DigitalService />
+  <GrowBusiness />
+  <Pricing />
+  <Product />
+  <Testimonial />
+  <About />
+  <New />
+  <Contact />
+  <Footer />
+</template>
+
+<style scoped>
+</style>
