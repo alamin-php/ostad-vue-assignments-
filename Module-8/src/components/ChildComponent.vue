65a5ccc2e65da8268a00c2ab6ec0c2f53427041c
alamin-php <alamin.php100@gmail.com>
2023-08-20 22:50:36 +0600

    Module-8 Added

---
+++ Module-8/src/components/ChildComponent.vue
@@ -0,0 +1,17 @@
+<script setup>
+ const propsValue  = defineProps(['modelValue'])
+ defineEmits(['update:modelValue'])
+
+ function messageEmitted(){
+     return 'Message change from child';  
+ }
+</script>
+<template>
+       <h3 class="text-blue-700 text-xl my-5">Child Component</h3>
+       <p class="my-5">
+         {{ modelValue }}
+      </p>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-5 w-48" @click="$emit('update:modelValue', messageEmitted())">
+         Button
+       </button>
+</template>
---
+++ Module-8/src/components/ParentComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+      import ChildComponent from './ChildComponent.vue'  
+      import { ref } from 'vue';
+      const message  = ref("Hello from parent!");
+</script>
+
+<template>
+    <h2 class="text-blue-700 text-xl my-5">Parent Component</h2>
+    <p class="my-5">
+        {{ message }}
+    </p>
+    <ChildComponent v-model="message"/>
+</template>
