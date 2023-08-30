57f58fdfc39b6ab09ef12ce86f0ca9a10d914e4c
alamin-php <alamin.php100@gmail.com>
2023-08-30 12:38:53 +0600

    Module-9 added

---
+++ Module-9/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container mt-5">
+    <div class="card">
+      <img src="https://resources.pulse.icc-cricket.com/ICC/photo/2019/06/11/b9b54b97-85c9-47cf-b50c-6f55819fc875/GettyImages-1153246234.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+  </div>
+</template>
